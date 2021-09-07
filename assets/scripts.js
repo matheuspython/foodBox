8dc536dddea30d5d20204f22f4724f26eec312e5
matheus souza <ruivorobado@gmail.com>
2021-09-07 16:30:10 -0300

    funcionalidades de selecao completas

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,12 +1,29 @@
 function escolherFrango(){
   document.querySelector('#frango').style.borderColor ='green'
   document.querySelector('#carne').style.borderColor ='white'
-
 }
 
 function escolheCarne(){
   document.querySelector('#carne').style.borderColor ='green'
-  document.querySelector('#frango').style.borderColor ='white'
+  document.querySelector('#frango').style.borderColor ='white' 
+}
 
-  
+function escolheCoca(){
+  document.querySelector('#coca').style.borderColor ='green'
+  document.querySelector('#suco').style.borderColor ='white' 
+}
+
+function escolheSuco(){
+  document.querySelector('#suco').style.borderColor ='green'
+  document.querySelector('#coca').style.borderColor ='white' 
+}
+
+function escolhePudim(){
+  document.querySelector('#pudim').style.borderColor ='green'
+  document.querySelector('#sorvete').style.borderColor ='white' 
+}
+
+function escolheSorvete(){
+  document.querySelector('#sorvete').style.borderColor ='green'
+  document.querySelector('#pudim').style.borderColor ='white' 
 }
