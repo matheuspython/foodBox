48d798e1df23e1284ed69562c7f159ea2f582213
matheus souza <ruivorobado@gmail.com>
2021-09-07 16:16:30 -0300

    adicionando primeiras funcoes ao codigo js

---
+++ assets/scripts.js
@@ -0,0 +1,12 @@
+function escolherFrango(){
+  document.querySelector('#frango').style.borderColor ='green'
+  document.querySelector('#carne').style.borderColor ='white'
+
+}
+
+function escolheCarne(){
+  document.querySelector('#carne').style.borderColor ='green'
+  document.querySelector('#frango').style.borderColor ='white'
+
+  
+}
